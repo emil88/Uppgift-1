4b9e8e6b370ddce9b3cdd732b5e8afdfbd504eca
emil88 <em222je@student.lnu.se>
2013-12-28 15:14:19 +0100

    Börjat skapa enhetstester i Visual Studio.

---
+++ Triangel/kod/c#/Labb1-whitebox/Labb1-whiteboxTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Labb1_whitebox;
+
+namespace Labb1_whiteboxTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double[] input = new double[3];
+
+            //Skickar 3 värden av typen double till konstruktorn, får då objektet för att testa med.
+            Triangle testObj = new Triangle(input[0], input[1], input[2]);
+        }
+    }
+}
